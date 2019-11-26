a51899163d6dbc0abd8dc9fc74c95606ef92aece
Emerson Knapp <emerson.b.knapp@gmail.com>
2019-11-26 11:39:13 -0800

    WIP adding some resources
    Signed-off-by: Emerson Knapp <emerson.b.knapp@gmail.com>

---
+++ linaro-toolchain.cmake
@@ -0,0 +1,40 @@
+if("$ENV{TARGET_ARCH}" STREQUAL "" OR
+   "$ENV{CROSS_COMPILE}" STREQUAL "" OR
+   "$ENV{SYSROOT}" STREQUAL "" OR
+   "$ENV{ROS2_INSTALL_PATH}" STREQUAL "" OR
+   "$ENV{PYTHON_SOABI}" STREQUAL "")
+    message(FATAL_ERROR "Target environment variables not defined")
+endif()
+
+set(CMAKE_SYSTEM_NAME Linux)
+SET(CMAKE_SYSTEM_VERSION 1)
+set(CMAKE_SYSTEM_PROCESSOR aarch64)
+
+set(CMAKE_C_COMPILER $ENV{CROSS_COMPILE}gcc)
+set(CMAKE_CXX_COMPILER $ENV{CROSS_COMPILE}g++)
+
+set(CMAKE_SYSROOT $ENV{SYSROOT})
+set(CMAKE_FIND_ROOT_PATH $ENV{ROS2_INSTALL_PATH})
+# set(CMAKE_TRY_COMPILE_TARGET_TYPE STATIC_LIBRARY)
+# - "-DCMAKE_TRY_COMPILE_TARGET_TYPE=STATIC_LIBRARY"
+# - "-DCMAKE_FIND_ROOT_PATH=/root/ros/sysroot/root_path:/root/ros/install"
+# - "-DCMAKE_INSTALL_RPATH=/root/ros/sysroot/opt/ros/$ROS_DISTRO/lib"
+# - "-DCMAKE_INSTALL_RPATH_USE_LINK_PATH=TRUE"
+set(CMAKE_FIND_ROOT_PATH_MODE_PROGRAM NEVER)
+set(CMAKE_FIND_ROOT_PATH_MODE_PACKAGE ONLY)
+set(CMAKE_FIND_ROOT_PATH_MODE_LIBRARY ONLY)
+set(CMAKE_FIND_ROOT_PATH_MODE_INCLUDE ONLY)
+
+# Specify the python SOABI
+set(PYTHON_SOABI $ENV{PYTHON_SOABI})
+
+if(NOT TARGET Qt5::moc)
+  set(QT_MOC_EXECUTABLE /usr/bin/moc)
+  add_executable(Qt5::moc IMPORTED)
+  set_property(TARGET Qt5::moc PROPERTY IMPORTED_LOCATION ${QT_MOC_EXECUTABLE})
+endif()
+
+# This assumes that pthread will be available on the target system
+# (this emulates that the return of the TRY_RUN is a return code "0")
+set(THREADS_PTHREAD_ARG "0"
+  CACHE STRING "Result from TRY_RUN" FORCE)
