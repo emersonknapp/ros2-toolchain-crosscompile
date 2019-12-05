ce27bba8cd8ca660210238ab5f11f4d4f8264323
Emerson Knapp <emerson.b.knapp@gmail.com>
2019-12-05 15:09:06 -0800

    Kinesis video streamer is building

--- cross-compile-deps.bash
+++ cross-compile-deps.bash
@@ -21,7 +21,14 @@ export CXXFLAGS="--sysroot $CROSS_COMPILE_SYSROOT"
 export CPPFLAGS="--sysroot $CROSS_COMPILE_SYSROOT"
 export LDFLAGS="--sysroot $CROSS_COMPILE_SYSROOT"
 
+BUILD_ZLIB=1
+BUILD_SSL=1
+BUILD_CURL=1
+BUILD_LOG=1
+BUILD_XML=1
+
 # Zlib
+if [ $BUILD_ZLIB = 1 ]; then
 wget -c https://zlib.net/zlib-1.2.11.tar.gz
 tar xzf zlib-1.2.11.tar.gz
 pushd zlib-1.2.11
@@ -29,8 +36,11 @@ pushd zlib-1.2.11
 make -j
 make install
 popd
+cp -r $CROSS_INSTALL_PREFIX/. $CROSS_COMPILE_SYSROOT
+fi
 
 # openssl
+if [ $BUILD_SSL = 1 ]; then
 wget -c https://www.openssl.org/source/openssl-1.1.1c.tar.gz
 tar xzf openssl-1.1.1c.tar.gz
 pushd openssl-1.1.1c
@@ -39,19 +49,25 @@ pushd openssl-1.1.1c
 make CC=$CC LD=$LD AR=$AR AS=$AS NM=$NM RANLIB=$RANLIB -j
 make install_sw CC=$CC LD=$LD AR=$AR AS=$AS NM=$NM RANLIB=$RANLIB
 popd
+cp -r $CROSS_INSTALL_PREFIX/. $CROSS_COMPILE_SYSROOT
+fi
 
 # curl
+if [ $BUILD_CURL = 1 ]; then
 wget -c https://curl.haxx.se/download/curl-7.65.1.tar.gz
 tar xzf curl-7.65.1.tar.gz
 pushd curl-7.65.1
-./configure --prefix=$CROSS_INSTALL_PREFIX --host=aarch64-linux --with-zlib --with-ca-path=/etc/ssl/certs --with-ca-bundle=/etc/ssl/certs/ca-certificates.crt
+./configure --prefix=$CROSS_INSTALL_PREFIX --host=aarch64-linux --with-ssl --with-zlib --with-ca-path=/etc/ssl/certs --with-ca-bundle=/etc/ssl/certs/ca-certificates.crt
 make -j
 make install
 popd
+cp -r $CROSS_INSTALL_PREFIX/. $CROSS_COMPILE_SYSROOT
+fi
 
 ## dependencies for kinesis video producer
 
 # log4cplus
+if [ $BUILD_LOG = 1 ]; then
 wget -c https://github.com/log4cplus/log4cplus/releases/download/REL_1_1_2/log4cplus-1.1.2.tar.gz
 tar xzf log4cplus-1.1.2.tar.gz
 pushd log4cplus-1.1.2
@@ -59,9 +75,21 @@ pushd log4cplus-1.1.2
 make -j
 make install
 popd
+cp -r $CROSS_INSTALL_PREFIX/. $CROSS_COMPILE_SYSROOT
+fi
 
 # TODO(EKNAPP) - the headers maybe should get installed to $SYSROOT/usr/include/ or the compiler fixed? It doesn't look in the right place by default
 
+# tinyxml2
+if [ $BUILD_XML = 1 ]; then
+wget -c -O tinyxml2-7.0.0.tar.gz https://github.com/leethomason/tinyxml2/archive/7.0.0.tar.gz
+tar xzf tinyxml2-7.0.0.tar.gz
+pushd tinyxml2-7.0.0
+make
+make install DESTDIR=$CROSS_INSTALL_PREFIX
+popd
+cp -r $CROSS_INSTALL_PREFIX/. $CROSS_COMPILE_SYSROOT
+fi
 
 # libgstreamer1.0-dev
 # wget -O gstreamer-1.15.90.tar.gz https://github.com/GStreamer/gstreamer/archive/1.15.90.tar.gz
